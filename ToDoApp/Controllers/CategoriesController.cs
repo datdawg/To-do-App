fc8bb7977c6ae808195e86b7cff278bd079be1e2
datdawg <klaus.munk.jensen@gmail.com>
2016-09-08 11:22:05 +0200

    Generic updates

--- ToDoApp/Controllers/CategoriesController.cs
+++ ToDoApp/Controllers/CategoriesController.cs
@@ -32,5 +32,23 @@ namespace ToDoApp.Controllers
 
             return Ok(model);
         }
+
+        [HttpDelete]
+        //[Authorize]
+        public IHttpActionResult DeleteCategory(int Id)
+        {
+            Category category = _db.Categories.Where(p => p.Id == Id).FirstOrDefault();
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Tasks.RemoveRange(category.Tasks);
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+
+            return Ok(category);
+        }
     }
 }
--- ToDoApp/Scripts/global.js
+++ ToDoApp/Scripts/global.js
@@ -1,4 +1,28 @@
 ﻿$(document).ready(function () {
+
+    $("#frm-create-category").validate();
+
+    function validateThis() {
+        bGo = false;
+        $("#frm-create-category").validate({            
+            invalidHandler: function (event, validator) {
+                // 'this' refers to the form
+                var errors = validator.numberOfInvalids();
+                if (errors) {
+                    bGo = false;
+                } else {
+                    bGo = true;
+                }
+            }
+        });
+        if (!bGo) {
+            alert('not valid');
+            return false;
+        } else {
+            alert('good to go');
+        }
+    }
+
     LoadCategoriesAndTasks();
 
     $(document)
@@ -13,27 +37,42 @@
                 method: "DELETE"
             }).done(function () {
                 oThis.closest('.task-container').remove();
-                $.notify('Opgave slettet', 'warn');
+                $.notify('Opgave slettet', 'info');
             });
         }
     })
     //complete task
     .delegate('.finishTask', 'click', function () {
-        oThis = $(this);
-        sHTML = $(oThis).closest('.title').html();
-        var id = $(this).closest('.task').data('id');
+        var result = confirm('Opgave fuldført?');
+        if (result == true) {
+            oThisTask = $(this).closest('.task');
+            oThis = $(this);
+            var id = $(this).closest('.task').data('id');
+            sText = $('.task[data-id="' + id + '"] .title').text();
 
-        console.log($(oThis).closest('.title').html());
-        alert('fix this');
-        return false;
+            $.ajax({
+                url: "api/tasks/" + id,
+                method: "PUT"
+            }).done(function () {
+                oThisTask.addClass('finished');
+                $.notify('Opgaven "' + sText + '" fuldført!', 'success');
+            });
+        }
+    })
+    //delete category and the tasks it contains
+    .delegate('.deleteCategory', 'click', function () {
+        var result = confirm('Vil du slette denne kategori og de evt. tilhørende opgaver?');
+        if (result == true) {
+            var id = $(this).data('catid');
 
-        $.ajax({
-            url: "api/tasks/" + id,
-            method: "PUT"
-        }).done(function () {
-            $(oThis).closest('.task').addClass("finished");
-            $.notify(sHTML + ": Check :)", 'success');
-        });
+            $.ajax({
+                url: "api/categories/" + id,
+                method: "DELETE"
+            }).done(function () {
+                LoadCategoriesAndTasks();
+                $.notify("Kategori slettet", 'info');
+            });
+        }
     });
 
     //create task
@@ -46,13 +85,16 @@
         }).done(function (data) {
             $('#createTask').modal('hide');
             $('.modal-backdrop').hide();
+            $('#Name').val('');
             LoadCategoriesAndTasks();
-            $.notify("Ny opgave tilføjet"), 'success';
+            $.notify("Ny opgave tilføjet", 'success');
         })
     });
-
     //create category
-    $('#btn-create-category').on('click', function () {
+    $('#btn-create-category').on('click', function (e) {
+
+        validateThis();
+        
         var data = $('#frm-create-category').serialize();
         $.ajax({
             url: "api/categories",
@@ -61,6 +103,7 @@
         }).done(function (data) {
             $('#createCategory').modal('hide');
             $('.modal-backdrop').hide();
+            $('#categoryName').val('');
             LoadCategoriesAndTasks();
             $.notify("Ny kategori oprettet", 'success');
         })
@@ -84,6 +127,9 @@ function LoadCategoriesAndTasks() {
             sAppendString += '<div class="header-column">';
             sAppendString += '<header>';
             sAppendString += '<div class="category" data-time="Today">';
+            sAppendString += '<div class="icon delete-category">';
+            sAppendString += '<span class="deleteCategory glyphicon glyphicon-trash" data-catid="' + iCatId + '"></span>';
+            sAppendString += '</div>';
             sAppendString += '<h2>' + sCatName + '</h2>';
             sAppendString += '</div>';
             sAppendString += '<a class="add-inline" data-catid="' + iCatId + '" data-toggle="modal" data-target="#createTask">';
@@ -117,28 +163,28 @@ function LoadCategoriesAndTasks() {
                 sAppendString += '</div></div>';
             });
 
-            $('.add-inline').on('click', function () {
-                $('#categoryId').attr('value', ($(this).data("catid")));
-            });
-
             sAppendString += '</div>';
             sAppendString += '</section>';
 
             $('.columns-container').append(sAppendString);
 
-            $(".items-column").sortable({
-                connectWith: ".items-column",
-                placeholder: "ui-state-highlight",
 
-                receive: function (event, ui) {
-                    iTaskId = ui.item.find('.task').data('id');
-                    oTarget = event.target;
-                    iCatId = $(oTarget).closest('section').data('catid');
-
-                    ChangeTaskCategory(iTaskId, iCatId);
-                }
-            }).disableSelection();
         });
+        $('.add-inline').on('click', function () {
+            $('#categoryId').attr('value', ($(this).data("catid")));
+        });
+        $(".items-column").sortable({
+            connectWith: ".items-column",
+            placeholder: "ui-state-highlight",
+
+            receive: function (event, ui) {
+                iTaskId = ui.item.find('.task').data('id');
+                oTarget = event.target;
+                iCatId = $(oTarget).closest('section').data('catid');
+
+                ChangeTaskCategory(iTaskId, iCatId);
+            }
+        }).disableSelection();
     });
 }
 
@@ -149,6 +195,6 @@ function ChangeTaskCategory(taskId, catId) {
         url: 'api/tasks/changecategory/' + taskId + '/' + catId,
         method: "PUT"
     }).done(function (data) {
-        
+
     })
 }
